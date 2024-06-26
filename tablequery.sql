06b6ded8fa0a342ecd76b6a344484003d81e669b
ktwlniranjan <61112099+ktwlniranjan@users.noreply.github.com>
2024-06-25 17:39:09 -0700

    Add files via upload
    main data source (dim and fact tables) in csv format with validation requirement docx, data model mapping file.

---
+++ tablequery.sql
@@ -0,0 +1,26 @@
+USE Practice
+
+CREATE TABLE CUSTOMERS(
+						Sex VARCHAR(6),
+						Fulldate DATE,
+						Age INT,
+						Social INT,	
+						First VARCHAR(50),
+						Middle	VARCHAR(50),
+						Last	VARCHAR(50),
+						Phone INT,	
+						Email VARCHAR(100),	
+						Address_1	VARCHAR(100),
+						Address_2	VARCHAR(100),
+						City VARCHAR(50),	
+						State_Name VARCHAR(50),
+						County VARCHAR(20),	
+						State VARCHAR(2),	
+						Zipcode	INT,
+						Marital_Status VARCHAR(50),	
+						Education VARCHAR(50),	
+						Proficiency	VARCHAR(50),
+						Nationality	VARCHAR(50),
+						Income VARCHAR(50)
+
+)
